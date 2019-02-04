fc5f42a36d07940eb3747984dc75b602af6085ed
anshika-2000 <anshika18csu047@ncuindia.edu>
2019-02-04 11:19:23 +0530

    update

---
+++ classIntroduction/program/src/Employee.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Employee{
+	   String name;
+    String address;
+    int yearOfJoining;
+    Employee1(){
+        name = "";
+        address = "";
+        yearOfJoining = 0;
+    }
+        void displayInfo()
+        {
+            System.out.println(this.name+"\t\t"+this.yearOfJoining+"\t\t"+this.address+"\n");
+        }
+    }
